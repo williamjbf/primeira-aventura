b7ad1670b8f1160fb5e4bc257cdf2912c37ef6d7
williamjbf <williamjbf@gmail.com>
2025-07-14 02:04:43 -0300

    Adicionado storybook ao projeto

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.getElementById('root')?.classList.add('bg-gray-900')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
